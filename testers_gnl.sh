3052c64a41792ae9425cd84cd4bd0537b184e35b
Lucas Lemes <llemes-f@student.42sp.org.br>
2021-04-11 17:39:31 -0300

    Ajuste para script de testes

--- testers_gnl.sh
+++ testers_gnl.sh
@@ -9,6 +9,14 @@ norminette *.c *.h
 git clone https://github.com/Mazoise/42TESTERS-GNL.git
 cd ..
 git clone https://github.com/mrjvs/42cursus_gnl_tests.git
+git clone https://github.com/charMstr/GNL_lover.git
+cd get_next_line
+cp get_next_line_utils.c get_next_line.c get_next_line.h ../GNL_lover/copy_in_here_GNL_files
+cd ..
+git clone https://github.com/Hellio404/Get_Next_Line_Tester.git
+cd get_next_line
+git clone https://github.com/Tripouille/gnlTester.git
+cd ..
 
 # RUN 42TESTERS-GNL
 cd get_next_line/GET42TESTERS-GNL
@@ -21,17 +29,20 @@ cd 42cursus_gnl_tests
 make 
 cd ..
 
-# RUN libft-war-machine
-#cd libft-war-machine
-#./grademe.sh
-#sed -i '' "s/PATH_LIBFT=../PATH_LIBFT=..\/libft/" my_config.sh
-#./grademe.sh
-#cd ..
+# RUN GNL_lover
+cd GNL_lover
+./GNL_lover.sh
+cd ..
 
-# RUN libftTester
+# RUN GET NEXT LINE TESTER
 #cd libftTester
 # CONFIGURE MAKEFILE MANUALLY: FT_PATH=../libft
 # #make
+# cd ..
+
+# RUN gnlTester
+cd get_next_line/gnlTester
+make m
 # cd ..
 
 # CLEANUP
