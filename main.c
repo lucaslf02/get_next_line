23934b8940d811ea45be9abf0f6c7921b84df238
Lucas Lemes <llemes-f@student.42sp.org.br>
2021-04-14 19:53:14 -0300

    For pull

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: llemes-f <llemes-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/20 19:17:26 by llemes-f          #+#    #+#             */
-/*   Updated: 2021/04/11 15:47:45 by llemes-f         ###   ########.fr       */
+/*   Updated: 2021/04/13 21:17:22 by llemes-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int	main()
 	char	*line;
 
 	fd = open("/home/lucas/Desktop/42cursos/get_next_line/text.txt", O_RDONLY);
-	while ((ret = get_next_line(fd, &line)) > 0)
+	while ((ret = get_next_line(-1, &line)) > 0)
 	{
 		printf("[%d] %s\n", ret, line);
 		free(line);
